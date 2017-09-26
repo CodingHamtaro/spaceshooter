1abf98982f3fea75ae62f822239e7d168106cbf5
John Eric T. Orolfo <jorolfo@usautoparts.com>
2017-09-26 15:43:33 +0800

    removed the file server

--- main.js
+++ main.js
@@ -1,36 +1,10 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
 const url = require('url')
-const Inert = require('inert');
 
 let win;
 
-const Hapi = require('hapi');
-
-const server = new Hapi.Server();
-server.connection({ port: 1903, host: 'localhost' });
-
-server.register(Inert, () => {});
-
-server.route({
-    method: 'GET',
-    path: '/{param*}',
-    handler: {
-        directory: {
-            path: './app',
-            listing: true
-        }
-    }
-});
-
 createWindow = () => {
-    server.start((err) => {
-        if (err) {
-            throw err;
-        }
-        console.log(`Server running at: ${server.info.uri}`);
-    });
-
     win = new BrowserWindow({
         minWidth: 960,
         minHeight: 750,
@@ -42,8 +16,8 @@ createWindow = () => {
     });
     win.setMenu(null);
     win.loadURL(url.format({
-        pathname: '0.0.0.0:1903',
-        protocol: 'http',
+        pathname: path.join(__dirname, '/app/index.html'),
+        protocol: 'file',
         slashes: true
     }));
     win.webContents.openDevTools();
@@ -56,7 +30,6 @@ app.on('ready', createWindow);
 
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
-        server.stop()
         app.quit();
     }
 })
